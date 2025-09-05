a18eb058f033691acff465b7a420f40c406a87dd
Ivan Hribernik <cihribernik@gmail.com>
2025-09-05 10:02:23 -0300

    feat(app): remove manualy redoc imp

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
+from pathlib import Path
+
 from flask import Flask
 from flask_cors import CORS
 from flask_pydantic_spec import FlaskPydanticSpec
 
 from app.infrastructure.config import Config
 from app.infrastructure.web.middlewares.error import ErrorMiddleware
-from app.infrastructure.web.middlewares.redoc import register_redoc
 from app.infrastructure.web.routes import register_routes
 
 
@@ -19,7 +20,6 @@ def create_app():
 
     spec = FlaskPydanticSpec("flask", title="Blog API", version="1.0.0", openapi=True)
     spec.register(app)
-    register_redoc(app, spec_url="/openapi.json", route="/redoc", title="Blog API Docs")
 
     return app
 
